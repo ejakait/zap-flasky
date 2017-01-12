dea9fa993b2a840c6c916954297a52fbdbab6f83
ejakait <ejakaitcaleb@outlook.com>
2017-01-12 17:10:00 +0300

    bot

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from flask import Flask
 
-app = Flask(__name__)
-@app.route('/')
-def hello():
-    return 'hello from zappa'
+import telepot, time 
+import os
+TOKEN = os.environ.get('BOT_TOKEN')
 
-if __name__ =='__main__':
-    app.run()
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+    print ('Got command: %s' % command)
+
+    if command == '/hello':
+        bot.sendMessage(chat_id, "Hello, how are you?")
+
+# Create a bot object with API key
+bot = telepot.Bot(TOKEN)
+bot.message_loop(handle)
+
+print('Listening ...')
+# Listen to the messages
+while 1:
+    time.sleep(5)
